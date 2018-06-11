5058a02cb7d60635e114cd5b9e9e2a279adff80b
Myungseo Kang <l3opold7@gmail.com>
2018-06-11 17:58:10 +0900

    DRF 세팅 및 게시판용 모델 추가

--- double_D/settings.py
+++ double_D/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts.apps.PostsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
